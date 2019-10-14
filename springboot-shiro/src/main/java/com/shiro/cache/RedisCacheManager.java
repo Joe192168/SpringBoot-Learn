1fd816da19db63b2c282edfec198ce9c4cb357da
= <=>
2019-10-14 17:44:58 +0800

    修改

--- springboot-shiro/src/main/java/com/shiro/cache/RedisCacheManager.java
+++ springboot-shiro/src/main/java/com/shiro/cache/RedisCacheManager.java
@@ -5,13 +5,6 @@ import org.apache.shiro.cache.CacheException;
 import org.apache.shiro.cache.CacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
 
-/**
- * @program: SpringBoot-Learn
- * @description:
- * @author: 肖乔辉
- * @create: 2019-10-14 13:31
- * @version: 1.0.0
- */
 public class RedisCacheManager implements CacheManager {
 
     @Autowired
