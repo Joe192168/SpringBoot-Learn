afd069c0f32ee3181b52f182bdd750683327d431
joe <joe192168@sina.com>
2019-10-15 13:52:43 +0800

    自动登录

--- springboot-shiro/src/main/java/com/shiro/vo/User.java
+++ springboot-shiro/src/main/java/com/shiro/vo/User.java
@@ -11,6 +11,7 @@ public class User {
 
     private String username;
     private String password;
+    private boolean rememberMe;
 
     public String getUsername() {
         return username;
@@ -26,5 +27,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public boolean isRememberMe() {
+        return rememberMe;
+    }
+
+    public void setRememberMe(boolean rememberMe) {
+        this.rememberMe = rememberMe;
     }
 }
