3228080a9ab0e6b81928163a51bf83644d27fea1
=Joe <=joe192168@sina.com>
2019-11-04 09:45:26 +0800

    mybatisplus

--- springmvc-mybatisplus/src/main/java/com/joe/controller/UserController.java
+++ springmvc-mybatisplus/src/main/java/com/joe/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.joe.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.joe.entity.User;
+import com.joe.entity.vo.LayerJson;
+import com.joe.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
- * 人员信息 前端控制器
+ *  前端控制器
  * </p>
  *
  * @author Joe
@@ -16,6 +26,73 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private IUserService usersService;
+
+    @RequestMapping(value = "/add-edit/{id}",method = RequestMethod.GET)
+    public String addOrEdit(@PathVariable Integer id, Model model){
+        if(id!=-1){
+            User user = usersService.selectById(id);
+            model.addAttribute("user",user);
+        }
+        return "user/add-edit";
+    }
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+
+
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public String list(){
+        return "user/list";
+    }
+
+    /**
+     * 使用Layui table完成分页功能
+     * @param pageIndex Layui table 默认提交当前页码的key 是page
+     * @param pageSize Layui table 默认提交每页显示条数的key 是limit
+     * @param search  查询条件
+     * @return  返回自己组装符合Layui table格式的Json数据
+     * @throws JsonProcessingException
+     */
+    @ResponseBody
+    @RequestMapping(value = "/list",method = RequestMethod.POST)
+    public String list(@RequestParam(value = "page",defaultValue = "1") Integer pageIndex, @RequestParam(value = "limit" , defaultValue = "10") Integer pageSize, String search) throws JsonProcessingException {
+        //查询页码和每页显示的条数
+        Page page = new Page(pageIndex,pageSize);
+        //查询条件
+        EntityWrapper<User> ew = new EntityWrapper<>();
+        ew.orderBy("id").or().like("id",search).or().like("username",search);
+
+        //根据查询条件查询符合的数据LayerJson
+        Page<User> mapPage = usersService.selectPage(page, ew);
+
+        //组装JSON对象数据
+        LayerJson layerJson = LayerJson.getInstance(mapPage.getRecords(),mapPage.getTotal());
+        //转为JSON字符串
+        return new ObjectMapper().writeValueAsString(layerJson);
+    }
+
+    @RequestMapping(value = "/add-edit",method = RequestMethod.POST)
+    public ResponseEntity addOrEdit(User user){
+        boolean flg = usersService.insertOrUpdate(user);
+        if(flg){
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
+
+    @RequestMapping(value = "/del/{id}",method = RequestMethod.POST)
+    public ResponseEntity del(@PathVariable Integer id){
+        boolean flg = usersService.deleteById(id);
+        if(flg){
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
 
 }
 
--- springmvc-mybatisplus/src/main/webapp/WEB-INF/pages/index.jsp
+++ springmvc-mybatisplus/src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%
     String path = request.getContextPath();
@@ -11,21 +10,5 @@
 </head>
 <body>
     <a href="user/index">显示用户信息</a>
-=======
-<%--
-  Created by IntelliJ IDEA.
-  User: Lenovo
-  Date: 2019/10/31
-  Time: 19:42
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>Title</title>
-</head>
-<body>
-
->>>>>>> origin/master
 </body>
 </html>
