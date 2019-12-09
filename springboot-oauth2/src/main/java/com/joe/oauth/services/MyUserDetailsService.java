11415cbd2c65c56b2c361b44f68c138334da80f4
=Joe <=joe192168@sina.com>
2019-12-09 10:03:48 +0800

    提供oauth2加密方法

--- springboot-oauth2/src/main/java/com/joe/oauth/services/MyUserDetailsService.java
+++ springboot-oauth2/src/main/java/com/joe/oauth/services/MyUserDetailsService.java
@@ -58,4 +58,9 @@ public class MyUserDetailsService implements UserDetailsService {
 
         return myUser;
     }
+
+    public String encode(String str){
+        return this.passwordEncoder.encode(str);
+    }
+
 }
--- springboot-oauth2/src/test/java/com/joe/oauth/OauthApplicationTests.java
+++ springboot-oauth2/src/test/java/com/joe/oauth/OauthApplicationTests.java
@@ -1,13 +1,19 @@
 package com.joe.oauth;
 
+import com.joe.oauth.services.MyUserDetailsService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OauthApplicationTests {
 
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
     @Test
     void contextLoads() {
+        System.out.println("加密："+myUserDetailsService.encode("order123"));
     }
 
 }
