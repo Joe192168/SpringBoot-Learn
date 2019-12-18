799d0bb3dbfaa4638c88a2befa7b94925d597d6d
=Joe <=joe192168@sina.com>
2019-12-18 14:37:04 +0800

    docker update

---
+++ springboot-markdocker/src/main/java/com/joe/makerdocker/MakerDockerApplication.java
@@ -0,0 +1,13 @@
+package com.joe.makerdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakerDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakerDockerApplication.class, args);
+    }
+
+}
---
+++ springboot-markdocker/src/test/java/com/joe/makerdocker/MakerDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joe.makerdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakerDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
