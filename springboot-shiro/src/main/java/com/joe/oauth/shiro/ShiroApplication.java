f293cec6b788b20ca9cb407e17de937c19ce1bfd
=Joe <=joe192168@sina.com>
2020-06-24 11:35:27 +0800

    修改

--- springboot-shiro/src/main/java/com/joe/oauth/shiro/ShiroApplication.java
+++ springboot-shiro/src/main/java/com/joe/oauth/shiro/ShiroApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.joe.shiro.mapper")
+@MapperScan("com.joe.oauth.shiro.mapper")
 @SpringBootApplication
 public class ShiroApplication {
 
